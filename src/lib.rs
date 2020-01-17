0ea3c62f9e8a55fd79f5668c45a2d8626501d0d5
knokko <knokogator@hotmail.com>
2020-01-17 19:57:26 +0100

    Process the refactoring of Layer from struct to trait

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,7 @@ pub fn main() {
 }
 
 fn create_main_menu(font: Rc<Font>) -> Rc<RefCell<dyn Container>> {
-    let mut layer = Layer::new(Some(Color::from_rgb(100, 200, 50)));
+    let mut layer = SimpleLayer::new(Some(Color::from_rgb(100, 200, 50)));
 
     let font_clone = Rc::clone(&font);
     add_simple_text_button(&mut layer, -8000, 5000, -4000, 7000, "Simple components", Color::BLUE, &font, TextAlignment::Center, move |agent, _, _| {
@@ -59,11 +59,11 @@ fn create_main_menu(font: Rc<Font>) -> Rc<RefCell<dyn Container>> {
         agent.change_container(create_overlapping_edit_menu(Rc::clone(&font_clone)));
     });
 
-    Rc::new(RefCell::new(FlatContainer::new(layer)))
+    Rc::new(RefCell::new(FlatContainer::new(Box::new(layer))))
 }
 
 fn create_simple_menu(font: Rc<Font>) -> Rc<RefCell<dyn Container>> {
-    let mut layer = Layer::new(Some(Color::from_rgb(100, 200, 50)));
+    let mut layer = SimpleLayer::new(Some(Color::from_rgb(100, 200, 50)));
 
     let font_clone = Rc::clone(&font);
     add_simple_text_button(&mut layer, -8000, 5000, -7000, 7000, "Back", Color::BLUE, &font, TextAlignment::Center, move |agent, _, _| {
@@ -72,13 +72,13 @@ fn create_simple_menu(font: Rc<Font>) -> Rc<RefCell<dyn Container>> {
 
     add_simple_edit_field(&mut layer, -2000, -1500, 2000, 1500, "Type...", &font);
 
-    Rc::new(RefCell::new(FlatContainer::new(layer)))
+    Rc::new(RefCell::new(FlatContainer::new(Box::new(layer))))
 }
 
 fn create_overlapping_edit_menu(font: Rc<Font>) -> Rc<RefCell<dyn Container>> {
-    let mut layer1 = Layer::new(Some(Color::from_rgb(100, 200, 200)));
-    let mut layer2 = Layer::new(None);
-    let mut layer3 = Layer::new(None);
+    let mut layer1 = SimpleLayer::new(Some(Color::from_rgb(100, 200, 200)));
+    let mut layer2 = SimpleLayer::new(None);
+    let mut layer3 = SimpleLayer::new(None);
 
     let font_clone = Rc::clone(&font);
     add_simple_text_button(&mut layer1, -8000, 5000, -7000, 7000, "Back", Color::BLUE, &font, TextAlignment::Center, move |agent, _, _| {
@@ -89,5 +89,5 @@ fn create_overlapping_edit_menu(font: Rc<Font>) -> Rc<RefCell<dyn Container>> {
     add_simple_edit_field(&mut layer2, -3000, -3000, 3000, -1000, "Layer2", &font);
     add_simple_edit_field(&mut layer3, -3000, -2000, 3000, 0, "Layer3", &font);
 
-    Rc::new(RefCell::new(LayeredContainer::new(vec![layer1, layer2, layer3])))
+    Rc::new(RefCell::new(LayeredContainer::new(vec![Box::new(layer1), Box::new(layer2), Box::new(layer3)])))
 }
